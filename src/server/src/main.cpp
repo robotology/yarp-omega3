ca3cdbb4bb51890dde2c2d0ce37d392121ecdc47
Nicola Piga <nicolapiga@gmail.com>
2021-02-03 18:49:31 +0100

    Cleanup

--- src/server/src/main.cpp
+++ src/server/src/main.cpp
@@ -33,45 +33,6 @@ int main(int argc, char** argv)
 
     Server server;
     server.runModule(rf);
-    // std::cout << "Initialization " << dhdGetSystemName() << std::endl;
-
-    // if (drdOpen () < 0)
-    //     return EXIT_FAILURE;
-
-    // if (!drdIsSupported ())
-    //     return EXIT_FAILURE;
-
-    // std::cout << "Using device " << dhdGetSystemName() << std::endl;
-
-    // if (!drdIsInitialized () && drdAutoInit () < 0)
-    //     return EXIT_FAILURE;
-    // else if (drdStart () < 0)
-    //     return EXIT_FAILURE;
-
-    // std::cout << "Test position control." << std::endl;
-
-    // drdRegulatePos(true);
-    // drdMoveToPos(0.0, 0.0, 0.0, false);
-
-    // std::this_thread::sleep_for (std::chrono::seconds(5));
-
-    // std::cout << "Test force control." << std::endl;
-    // drdRegulatePos(false);
-    // drdRegulateGrip(false);
-    // drdRegulateRot(false);
-    // drdEnableFilter(false);
-
-    // // while (true)
-    // // {
-    // //     std::this_thread::sleep_for (std::chrono::milliseconds(10));
-    // //     drdSetForceAndTorqueAndGripperForce(0.0, 0.0, 5.0, 0.0, 0.0, 0.0, 0.0);
-    // // }
-
-    // std::this_thread::sleep_for (std::chrono::seconds(5));
-
-    // std::cout << "Closing." << std::endl;
-    // drdStop ();
-    // drdClose ();
 
     return EXIT_SUCCESS;
 }
