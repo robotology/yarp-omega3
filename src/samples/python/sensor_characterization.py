4e3f2b73fea03d69dc94470c5b91bac3bd55f145
Simon Mueller-Cleve <s.mueller-cleve@gmx.de>
2021-02-04 15:01:36 +0100

    Inlcuded position and force read out

--- src/samples/python/sensor_characterization.py
+++ src/samples/python/sensor_characterization.py
@@ -48,80 +48,58 @@ def send_stop(rpc):
 
 def main():
 
+    # move that config
     yarp.Network.init()
 
-    # connect to port for sending commands
-    rpc_client_commands = yarp.RpcClient()
-    rpc_client_commands.open('/commands-omega/rpc:o')
-    yarp.NetworkBase.connect('/commands-omega/rpc:o', '/yarp-omega3-server/rpc:i', "fast_tcp")
+    # setting up connection for sending commands
+    rpc_client = yarp.RpcClient()
+    rpc_client.open('/server-omega/rpc:o')
+    yarp.NetworkBase.connect('/server-omega/rpc:o', '/yarp-omega3-server/rpc:i', "fast_tcp")
+    if not yarp.NetworkBase.connect('/server-omega/rpc:o', '/yarp-omega3-server/rpc:i', "fast_tcp"):
+        print("Failed to connect to server")
 
-    # connect to port for recieving position
-    rpc_client_position = yarp.RpcClient()
-    rpc_client_position.open("/position_input:o")
-    yarp.NetworkBase.connect("/yarp-omega3-position/rpc:o", "/position_input:o", "fast_tcp")
+    # setting up connection for recieving position stream
+    position_stream = yarp.BufferedPortVector() # BufferedPortProperty instead of port?
+    position_stream.open("/position_input:o")
+    yarp.Network.connect("/yarp-omega3-server/position:o", "/position_input:o", "fast_tcp")
+    if not yarp.Network.connect("/yarp-omega3-server/position:o", "/position_input:o", "fast_tcp"):
+        print("Failed to connect to yarp port for position input")
 
-    # connect to port for recieving force
-    rpc_client_position = yarp.RpcClient()
-    rpc_client_position.open("/position_input:o")
-    yarp.NetworkBase.connect("/yarp-omega3-force/rpc:o", "/position_input:o", "fast_tcp")
-    
+    # setting up connection for recieving force stream
+    force_stream = yarp.BufferedPortVector()
+    force_stream.open("/force_input:o")
+    yarp.NetworkBase.connect("/yarp-omega3-server/force:o", "/force_input:o", "fast_tcp")
+    if not yarp.NetworkBase.connect("/yarp-omega3-server/force:o", "/force_input:o", "fast_tcp"):
+        print("Failed to connect to yarp port for force input")
+
+    # update loop
     try:
-        send_stop(rpc_client_commands)
-        print("**********************************")
-        print("*Move the robot to start position*")
-        print("*and press ENTER to confirm      *" ) 
-        print("**********************************")
+        send_stop(rpc_client) # let the robot be moved freely
+
+        print("**************************************")
+        print("*Move the robot to start position    *")
+        print("*and press ENTER to start experiment.*") 
+        print("*The procedure can be interrupted at *")
+        print("*anytime by pressing CTRL + C.       *")
+        print("**************************************")
         
         # case one: init position
         #if (input()=='s'):
         # move omega by hand to initial to position and store coordinates when button pressed
         # wait for keybord input to save ref positions
-        input()
-        print("ENTER pressed!")
+        # input()
+
         # read in from yarp port
-        print("got the position")
+        positions = position_stream.read()
+        print(positions[0], positions[1], positions[2])
+        forces = force_stream.read()
+        print(forces[0], forces[1], forces[2])
 
-        # x_ref = get_position_reference(rpc_client_commands, x, y, z)[2]
-        # y_ref = get_position_reference(rpc_client_commands, x, y, z)[3]
-        # z_ref = get_position_reference(rpc_client_commands, x, y, z)[4]
 
-        # # move up until no contact + tol. -> safe coordinates (x_ini, y_ini, z_ini)
-        # while (get_force_reference(rpc_client_commands, f_x, f_y, f_z)[4] > 0):
-        #     send_position_reference(
-        #         rpc_client_commands, x_ref, y_ref, z_ref+counter/1000)
-        # # increase distnace by tol to be sure no contact is made even after hysteresis
-        # # and store values
-        # z_ref = z_ref+tol+counter/1000
-        # send_position_reference(rpc_client_commands, x_ref, y_ref, z_ref)
-
-        # # case two: apply forces
-        # #if (input()=='e'):
-        # # loop over different forces applied to fingertip by omega
-        # for force in range(f_z_init, max_force, step_size):
-        #     # move slowly down to make contact and go on moving until goal force reached
-        #     send_position_reference(
-        #         rpc_client_commands, x_ref, y_ref, z_ref-step_size_position)
-        #     get_force_reference(rpc_client_commands, f_x, f_y, f_z)
-        #     counter = counter+1
-        #     # when goal force reached hold for certain time
-        #     if (get_force_reference(rpc_client_commands, f_x, f_y, f_z)[4] >= force):
-        #         # read time here: time_pre_start
-        #         start = timeit.default_timer()
-        #         while(timeit.default_timer() <= start + time_to_press):
-        #             # read time here again
-        #             send_position_reference(
-        #                 rpc_client_commands, x_ref, y_ref, z_ref-step_size_position)
-        #     # keep force const. for n time
-        #     # dump data (yarp data dumper)
-        #     # move to safed coordinates (x_ini, y_ini, z_ini)
-
-        # #if (input()=='q'):
-        # send_stop(rpc_client_commands)
-        # rpc_client_commands.close()
 
     except (KeyboardInterrupt):
-        send_stop(rpc_client_commands)
-        rpc_client_commands.close()
+        send_stop(rpc_client)
+        rpc_client.close()
 
 
 if __name__ == '__main__':
